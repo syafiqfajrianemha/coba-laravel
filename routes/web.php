b80a480f05c3903a3486bfd327613c9da35777be
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-07-22 19:56:34 +0700

    Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Syafiq Fajrian Emha",
+        "email" => "sfemha@gmail.com"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
